e1a8fe656febdaafa1d4160795d969bc22dc79b1
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-02-01 02:16:32 +0100

    Add cleanup to compilation script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 xelatex cv.tex
 biber cv
 xelatex cv.tex
+find . -maxdepth 1 -type f ! -name '*.sh' ! -name '*.tex' ! -name '*.bib' ! -name '*.cls' ! -name '*.git' ! -name '*.gitignore' ! -name '*.pdf' -delete
+
