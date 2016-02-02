7edf939193d5a7798527e262a19a3546fa40cd8c
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-02-02 20:19:16 +0100

    Use a positional argument instead of hardcoded name

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
-xelatex cv.tex
+xelatex $1 
 biber cv
-xelatex cv.tex
+xelatex $1
 find . -maxdepth 1 -type f ! -name '*.sh' ! -name '*.tex' ! -name '*.bib' ! -name '*.cls' ! -name '*.git' ! -name '*.gitignore' ! -name '*.pdf' -delete
 
