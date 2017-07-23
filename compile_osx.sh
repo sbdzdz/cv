fd69b9a02647fbc78e39a8a4d103c6687a1d8168
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-07-23 16:54:06 +0200

    Add a new compile script, remove old files

--- compile_osx.sh
+++ compile_osx.sh
@@ -1,5 +1,2 @@
-xelatex $1 
-biber $1 
-xelatex $1
+echo 'R' | xelatex $1
 find . -maxdepth 1 -type f ! -name '*.sh' ! -name '*.tex' ! -name '*.bib' ! -name '*.cls' ! -name '*.git' ! -name '*.gitignore' ! -name '*.pdf' -delete
-
