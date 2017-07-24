a0f75440bd6e31533b8345ac73da91c62fcf7081
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-07-24 11:46:40 +0200

    Compile twice to ensure consistent effects.

--- compile_osx.sh
+++ compile_osx.sh
@@ -1,2 +1,3 @@
 echo 'R' | xelatex $1
+echo 'R' | xelatex $1
 find . -maxdepth 1 -type f ! -name '*.sh' ! -name '*.tex' ! -name '*.bib' ! -name '*.cls' ! -name '*.git' ! -name '*.gitignore' ! -name '*.pdf' -delete
