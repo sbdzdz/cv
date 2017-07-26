6539d8c3a060eab2d2ee7a0ad4f17d772edebc3d
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-07-26 22:11:47 +0200

    Include Linux fonts

--- compile_linux.sh
+++
@@ -1,5 +0,0 @@
-xelatex $1 
-biber $1 
-xelatex $1
-find . -maxdepth 1 -type f ! -name '*.sh' ! -name '*.tex' ! -name '*.bib' ! -name '*.cls' ! -name '*.git' ! -name '*.gitignore' ! -name '*.pdf' -delete
-
