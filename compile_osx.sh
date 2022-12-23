eabce74b3cd5f8b139838c2282fa7844e1aeffb4
Sebastian <sebastian.dziadzio@gmail.com>
2022-12-23 19:22:49 +0100

    Update the compilation script

--- compile_osx.sh
+++ compile_osx.sh
@@ -1,3 +1,3 @@
-echo 'R' | xelatex $1
-echo 'R' | xelatex $1
+echo 'R' | xelatex cv
+echo 'R' | xelatex cv
 find . -maxdepth 1 -type f ! -name '*.sh' ! -name '*.tex' ! -name '*.bib' ! -name '*.cls' ! -name '*.git' ! -name '*.gitignore' ! -name '*.pdf' -delete
